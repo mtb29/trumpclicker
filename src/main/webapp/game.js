738c9b00bbe2b9b9027e3894419f49a4ca4239a7
mtb29 <bradymt29@mansfield.edu>
2017-02-19 22:30:51 -0500

    basic game is functional

--- src/main/webapp/game.js
+++ src/main/webapp/game.js
@@ -1,2 +1,40 @@
+Game = {};
 
+Game.Launch = function ()
+{
+    Game.ready = 0;
+
+    Game.Init = function ()
+    {
+        Game.ready = 1;
+
+        /* Game variables */
+        Game.version = 0.1;
+        Game.totalCoins = 0;
+        Game.coinClicks = 0;
+        Game.coins = 0;
+
+        Game.ClickCoin = function ()
+        {
+            Game.coins++;
+            Game.coinClicks++;
+            Game.Update();
+        };
+        
+        Game.Update = function () 
+        {
+            document.getElementById('currentCoins').innerHTML = "Trump Coins: " + Game.coins;
+        };
+        
+        document.getElementById('trumpcoin').onclick = Game.ClickCoin;
+    };
+};
+
+Game.Launch();
+
+window.onload = function ()
+{
+    if (!Game.ready)
+        Game.Init();
+};
 
