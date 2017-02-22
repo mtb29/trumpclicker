84eb6456a6727db8eed85be0cc26093755c3061d
55bradymt29 <55bradymt29@MUP-C-MHHDB.PASSHE.LCL>
2017-02-22 15:15:25 -0500

    Added first building and coins per click functionality. Added placeholder loan building icon.

--- src/main/webapp/game.js
+++ src/main/webapp/game.js
@@ -10,23 +10,46 @@ Game.Launch = function ()
 
         /* Game variables */
         Game.version = 0.1;
-        Game.totalCoins = 0;
-        Game.coinClicks = 0;
         Game.coins = 0;
+        Game.coinsPerClick = 1;
+
+        /* Buildings */
+        Game.loan = 0;
+        Game.loanCost = 250;
 
         Game.ClickCoin = function ()
         {
-            Game.coins++;
-            Game.coinClicks++;
+            Game.coins += Game.coinsPerClick;
             Game.Update();
         };
-        
-        Game.Update = function () 
+
+        Game.ClickLoan = function ()
         {
-            document.getElementById('currentCoins').innerHTML = "Trump Coins: " + Game.coins;
+            if (Game.coins < Game.loanCost)
+            {
+                alert("You need " + Game.loanCost.toLocaleString() + " Trump Coins to purchase that building.");
+            } else
+            {
+                alert("You earned A Small Loan of a Million Dollars!");
+                Game.loan++;
+                Game.coins -= Game.loanCost;
+                Game.loanCost *= 1.1;
+                Game.loanCost = Math.round(Game.loanCost);
+                Game.Update();
+            }
         };
-        
+
+        Game.Update = function ()
+        {
+            Game.coinsPerClick = 1 + Game.loan;
+            document.getElementById('currentCoins').innerHTML = "Trump Coins: " + Game.coins.toLocaleString() + " (+" + Game.coinsPerClick.toLocaleString() + " CPC)";
+            document.getElementById('currentLoans').innerHTML = "Loans: " + Game.loan.toLocaleString() + " (+" + Game.loan.toLocaleString() + " CPC)";
+        };
+
         document.getElementById('trumpcoin').onclick = Game.ClickCoin;
+        document.getElementById("loan").onclick = Game.ClickLoan;
+        document.getElementById("header").innerHTML = "Trump Clicker<br/>Version: " + Game.version; 
+        Game.Update();
     };
 };
 
