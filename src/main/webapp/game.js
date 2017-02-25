53be78cfc3bed1027a0f698534e60fd79adcd56e
mtb29 <bradymt29@mansfield.edu>
2017-02-25 18:24:18 -0500

    added coins per second functionality, added win condition, added reset button

--- src/main/webapp/game.js
+++ src/main/webapp/game.js
@@ -12,22 +12,29 @@ Game.Launch = function ()
         Game.version = 0.1;
         Game.coins = 0;
         Game.coinsPerClick = 1;
+        Game.coinsPerSecond = 0;
+        Game.clicks = 0;
+        Game.seconds = 0;
 
-        /* Buildings */
+        /* Upgrades */
         Game.loan = 0;
         Game.loanCost = 250;
+        Game.executiveOrder = 0;
+        Game.executiveOrderCost = 1000;
+        Game.wall = 0;
+        Game.wallCost = 20000000000;
 
         Game.ClickCoin = function ()
         {
+            Game.clicks++;
             Game.coins += Game.coinsPerClick;
-            Game.Update();
         };
 
         Game.ClickLoan = function ()
         {
             if (Game.coins < Game.loanCost)
             {
-                alert("You need " + Game.loanCost.toLocaleString() + " Trump Coins to purchase that building.");
+                alert("You need " + Game.loanCost.toLocaleString() + " Trump Coins to purchase that upgrade.");
             } else
             {
                 alert("You earned A Small Loan of a Million Dollars! (-" + Game.loanCost + " Trump Coins)");
@@ -35,21 +42,57 @@ Game.Launch = function ()
                 Game.coins -= Game.loanCost;
                 Game.loanCost *= 1.1;
                 Game.loanCost = Math.round(Game.loanCost);
-                Game.Update();
+            }
+        };
+
+        Game.ClickOrder = function ()
+        {
+            if (Game.coins < Game.executiveOrderCost)
+            {
+                alert("You need " + Game.executiveOrderCost.toLocaleString() + " Trump Coins to purchase that upgrade.");
+            } else
+            {
+                alert("You earned An Executive Order by Mr. President himself (-" + Game.executiveOrderCost + " Trump Coins)");
+                Game.executiveOrder++;
+                Game.coins -= Game.executiveOrderCost;
+                Game.executiveOrderCost *= 1.5;
+                Game.executiveOrderCost = Math.round(Game.executiveOrderCost);
+            }
+        };
+
+        Game.ClickWall = function ()
+        {
+            if (Game.coins < Game.wallCost) {
+                alert("You need " + Game.wallCost.toLocaleString() + " Trump Coins to purchase that upgrade.");
+            } else {
+                Game.wall++;
+                Game.coins -= Game.wallCost;
+                Game.wallCost *= 2.5;
+                Game.wallCost = Math.round(Game.wallOrderCost);
+                window.open("winner.html", "_blank");
             }
         };
 
         Game.Update = function ()
         {
-            Game.coinsPerClick = 1 + Game.loan;
-            document.getElementById('currentCoins').innerHTML = "Trump Coins: " + Game.coins.toLocaleString() + " (+" + Game.coinsPerClick.toLocaleString() + " CPC)";
-            document.getElementById('currentLoans').innerHTML = "Loans: " + Game.loan.toLocaleString() + " (+" + Game.loan.toLocaleString() + " CPC)";
+            Game.seconds++;
+            Game.coinsPerClick = 1 + Game.loan + (Game.executiveOrder * 3);
+            Game.coinsPerSecond = 0 + (Game.loan * 2) + (Game.executiveOrder * 5);
+            Game.coins += Game.coinsPerSecond;
+            document.getElementById("currentCoins").innerHTML = "Trump Coins: " + Game.coins.toLocaleString() + " (+" + Game.coinsPerClick.toLocaleString() + " CPC/+" + Game.coinsPerSecond.toLocaleString() + " CPS)";
+            document.getElementById("currentLoans").innerHTML = "Loans: " + Game.loan.toLocaleString() + " (+" + Game.loan.toLocaleString() + " CPC/+" + (Game.loan * 2).toLocaleString() + " CPS)<br/>Price: " + Game.loanCost.toLocaleString() + " TC";
+            document.getElementById("currentExecutiveOrders").innerHTML = "Executive Orders: " + Game.executiveOrder.toLocaleString() + " (+" + (Game.executiveOrder * 3).toLocaleString() + " CPC/+" + (Game.executiveOrder * 5).toLocaleString() + " CPS)<br/>Price: " + Game.executiveOrderCost.toLocaleString() + " TC";
+            document.getElementById("currentWalls").innerHTML = "Build the Wall<br/>Price: " + Game.wallCost.toLocaleString() + " TC";
+            document.getElementById("stats").innerHTML = "Stats<br/><br/>Coin Clicks: " + Game.clicks.toLocaleString() + "<br/>Upgrades: " + (Game.loan + Game.executiveOrder) + "<br/>Seconds: " + Game.seconds.toLocaleString();
         };
 
-        document.getElementById('trumpcoin').onclick = Game.ClickCoin;
+        document.getElementById("trumpcoin").onclick = Game.ClickCoin;
         document.getElementById("loan").onclick = Game.ClickLoan;
-        document.getElementById("header").innerHTML = "Trump Clicker<br/>Version: " + Game.version; 
+        document.getElementById("executiveorder").onclick = Game.ClickOrder;
+        document.getElementById("thewall").onclick = Game.ClickWall;
+        document.getElementById("header").innerHTML = "Trump Clicker<br/>Version: " + Game.version;
         Game.Update();
+        setInterval(Game.Update, 1000);
     };
 };
 
