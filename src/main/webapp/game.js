feb4e457d05146a913a8e7e4fef5c323fdae8e08
mtb29 <bradymt29@mansfield.edu>
2017-04-12 14:48:32 -0400

    version 0.3: updated layout, added more sounds, another trophy

--- src/main/webapp/game.js
+++ src/main/webapp/game.js
@@ -7,7 +7,7 @@ Game = {};
 
 var Trophy = {};
 var TrophyText = {};
-var TotalTrophies = 9;
+var TotalTrophies = 10;
 var TrophyCount = 0;
 
 function secondsToTime(secs)
@@ -46,7 +46,25 @@ function play() {
     var audio = document.getElementById('trophysound');
     if (audio.paused) {
         audio.play();
-    }else{
+    } else {
+        audio.currentTime = 0;
+    }
+}
+
+function playmini() {
+    var audio = document.getElementById('minimesound');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.currentTime = 0;
+    }
+}
+
+function playcheat() {
+    var audio = document.getElementById('cheatsound');
+    if (audio.paused) {
+        audio.play();
+    } else {
         audio.currentTime = 0;
     }
 }
@@ -60,7 +78,7 @@ Game.Launch = function ()
         Game.ready = 1;
 
         /* Game variables */
-        Game.version = 0.2;
+        Game.version = 0.3;
         Game.coins = 0;
         Game.coinsPerClick = 1;
         Game.coinsPerSecond = 0;
@@ -69,23 +87,25 @@ Game.Launch = function ()
 
         /* Trophies */
         Trophy[0] = 0;
-        TrophyText[0] = "Hello, Mr. President: Earn your first Trump Coin";
+        TrophyText[0] = "<strong>Hello, Mr. President</strong>: Earn your first Trump Coin";
         Trophy[1] = 0;
-        TrophyText[1] = "Helping Hand: Earn your first Loan";
+        TrophyText[1] = "<strong>Helping Hand</strong>: Earn your first Loan";
         Trophy[2] = 0;
-        TrophyText[2] = "Sign Here, Initial There: Earn your first Executive Order";
+        TrophyText[2] = "<strong>Sign Here, Initial There</strong>: Earn your first Executive Order";
         Trophy[3] = 0;
-        TrophyText[3] = "You Made America Great Again: Build the wall";
+        TrophyText[3] = "<strong>You Made America Great Again</strong>: Build the wall";
         Trophy[4] = 0;
-        TrophyText[4] = "Cheaters Never Prosper...wait a doggone minute....: Use the cheat command successfully";
+        TrophyText[4] = "<strong>Cheaters Never Prosper...wait a doggone minute....</strong>: Use the cheat command successfully";
         Trophy[5] = 0;
-        TrophyText[5] = "Luck Of The Draw: You Got Lucky";
+        TrophyText[5] = "<strong>Luck Of The Draw</strong>: You Got Lucky";
         Trophy[6] = 0;
-        TrophyText[6] = "Permission Denied: Deport your first illegal immigrant";
+        TrophyText[6] = "<strong>Permission Denied</strong>: Deport your first illegal immigrant";
         Trophy[7] = 0;
-        TrophyText[7] = "The Meaning to Life, the Universe, and Everything: Buy 42 upgrades";
+        TrophyText[7] = "<strong>The Meaning to Life, the Universe, and Everything</strong>: Buy 42 upgrades";
         Trophy[8] = 0;
-        TrophyText[8] = "Hello, Comrade?: Make your first phone call."
+        TrophyText[8] = "<strong>Hello, Comrade?</strong>: Make your first phone call."
+        Trophy[9] = 0;
+        TrophyText[9] = "<strong>It's like me, but mini</strong>: Click on minime.";
 
         /* Upgrades */
         Game.loan = 0;
@@ -104,7 +124,6 @@ Game.Launch = function ()
             if (Trophy[0] === 0) {
                 Trophy[0] = 1;
                 TrophyCount++;
-                document.getElementById("toolTip").innerHTML = "ToolTip: Trophy: " + TrophyText[0];
                 play();
             }
 
@@ -173,7 +192,7 @@ Game.Launch = function ()
                 Game.deportCost = Math.round(Game.deportCost);
             }
         };
-        
+
         Game.ClickPhone = function ()
         {
             if (Game.coins < Game.phoneCallCost)
@@ -203,7 +222,6 @@ Game.Launch = function ()
                 if (Trophy[3] === 0) {
                     Trophy[3] = 1;
                     TrophyCount++;
-                    document.getElementById("toolTip").innerHTML = "ToolTip: Trophy: " + TrophyText[3];
                     play();
                 }
                 Game.wall++;
@@ -214,19 +232,30 @@ Game.Launch = function ()
             }
         };
 
+        Game.ClickMini = function ()
+        {
+            if (Trophy[9] === 0) {
+                Trophy[9] = 1;
+                TrophyCount++;
+                play();
+            } else {
+                playmini();
+            }
+        };
+
         Game.Update = function ()
         {
             Game.seconds++;
             Game.coinsPerClick = 1 + Game.loan + (Game.executiveOrder * 3) + (Game.deport * 4) + (Game.phoneCall * 5);
             Game.coinsPerSecond = 0 + (Game.loan * 2) + (Game.executiveOrder * 5) + (Game.deport * 6) + (Game.phoneCall * 7);
             Game.coins += Game.coinsPerSecond;
-            document.getElementById("currentCoins").innerHTML = "Trump Coins: " + Game.coins.toLocaleString() + " (+" + Game.coinsPerClick.toLocaleString() + " CPC/+" + Game.coinsPerSecond.toLocaleString() + " CPS)";
-            document.getElementById("currentLoans").innerHTML = "Small Loans: " + Game.loan.toLocaleString() + " (+" + Game.loan.toLocaleString() + " CPC/+" + (Game.loan * 2).toLocaleString() + " CPS)<br/>Price: " + Game.loanCost.toLocaleString() + " TC";
-            document.getElementById("currentExecutiveOrders").innerHTML = "Executive Orders: " + Game.executiveOrder.toLocaleString() + " (+" + (Game.executiveOrder * 3).toLocaleString() + " CPC/+" + (Game.executiveOrder * 5).toLocaleString() + " CPS)<br/>Price: " + Game.executiveOrderCost.toLocaleString() + " TC";
-            document.getElementById("currentDeports").innerHTML = "Immigrants Deported: " + Game.deport.toLocaleString() + " (+" + (Game.deport * 4).toLocaleString() + " CPC/+" + (Game.deport * 6).toLocaleString() + " CPS)<br/>Price: " + Game.deportCost.toLocaleString() + " TC";
-            document.getElementById("currentPhoneCalls").innerHTML = "Phone Calls: " + Game.phoneCall.toLocaleString() + " (+" + (Game.phoneCall * 5).toLocaleString() + " CPC/+" + (Game.phoneCall * 7).toLocaleString() + " CPS)<br/>Price: " + Game.phoneCallCost.toLocaleString() + " TC";
-            document.getElementById("currentWalls").innerHTML = "Walls Built: " + Game.wall.toLocaleString() + "<br/>Price: " + Game.wallCost.toLocaleString() + " TC";
-            document.getElementById("stats").innerHTML = "Stats<br/><br/>Coin Clicks: " + Game.clicks.toLocaleString() + "<br/>Upgrades: " + (Game.loan + Game.executiveOrder + Game.deport + Game.phoneCall) + "<br/>Time: " + secondsToTime(Game.seconds);
+            document.getElementById("currentCoins").innerHTML = "<strong>Trump Coins</strong>: " + Game.coins.toLocaleString() + " (+" + Game.coinsPerClick.toLocaleString() + " CPC/+" + Game.coinsPerSecond.toLocaleString() + " CPS)";
+            document.getElementById("currentLoans").innerHTML = "<strong>Small Loans</strong>: " + Game.loan.toLocaleString() + " (+" + Game.loan.toLocaleString() + " CPC/+" + (Game.loan * 2).toLocaleString() + " CPS)<br/>Price: " + Game.loanCost.toLocaleString() + " TC";
+            document.getElementById("currentExecutiveOrders").innerHTML = "<strong>Executive Orders</strong>: " + Game.executiveOrder.toLocaleString() + " (+" + (Game.executiveOrder * 3).toLocaleString() + " CPC/+" + (Game.executiveOrder * 5).toLocaleString() + " CPS)<br/>Price: " + Game.executiveOrderCost.toLocaleString() + " TC";
+            document.getElementById("currentDeports").innerHTML = "<strong>Immigrants Deported</strong>: " + Game.deport.toLocaleString() + " (+" + (Game.deport * 4).toLocaleString() + " CPC/+" + (Game.deport * 6).toLocaleString() + " CPS)<br/>Price: " + Game.deportCost.toLocaleString() + " TC";
+            document.getElementById("currentPhoneCalls").innerHTML = "<strong>Phone Calls</strong>: " + Game.phoneCall.toLocaleString() + " (+" + (Game.phoneCall * 5).toLocaleString() + " CPC/+" + (Game.phoneCall * 7).toLocaleString() + " CPS)<br/>Price: " + Game.phoneCallCost.toLocaleString() + " TC";
+            document.getElementById("currentWalls").innerHTML = "<strong>Walls Built</strong>: " + Game.wall.toLocaleString() + "<br/>Price: " + Game.wallCost.toLocaleString() + " TC";
+            document.getElementById("stats").innerHTML = "<strong>Stats</strong><br/><br/><strong>Coin Clicks</strong>: " + Game.clicks.toLocaleString() + "<br/><strong>Upgrades</strong>: " + (Game.loan + Game.executiveOrder + Game.deport + Game.phoneCall) + "<br/><strong>Time</strong>: " + secondsToTime(Game.seconds);
             Trophies();
         };
 
@@ -239,7 +268,6 @@ Game.Launch = function ()
                 if (Trophy[5] === 0) {
                     Trophy[5] = 1;
                     TrophyCount++;
-                    document.getElementById("toolTip").innerHTML = "ToolTip: Trophy: " + TrophyText[5];
                     play();
                 }
             }
@@ -248,21 +276,21 @@ Game.Launch = function ()
                 if ((Game.loan + Game.executiveOrder + Game.deport + Game.phoneCall) >= 42) {
                     Trophy[7] = 1;
                     TrophyCount++;
-                    document.getElementById("toolTip").innerHTML = "ToolTip: Trophy: " + TrophyText[7];
                     play();
                 }
             }
 
 
-            var text = "Trophies [" + TrophyCount + "/" + TotalTrophies + "]";
+            var text = "<strong>Trophies</strong> [" + TrophyCount + "/" + TotalTrophies + "]";
             for (i = 0; i <= TotalTrophies; i++) {
                 if (Trophy[i] === 1) {
-                    text = text + "<br/>[*] " + TrophyText[i];
+                    text = text + "<br/>[<img id='minime' src='img/trumpcoinmini.png' width='25' height='19' alt='*'/>] " + TrophyText[i];
                 }
             }
 
             if (TrophyCount > 0) {
                 document.getElementById("trophies").innerHTML = text;
+                document.getElementById("minime").onclick = Game.ClickMini;
             }
         };
 
@@ -272,6 +300,7 @@ Game.Launch = function ()
 
             if (rand === 1) {
                 document.getElementById("toolTip").innerHTML = "ToolTip: You're Fired! The game has been reset!";
+                playcheat();
                 setTimeout(function () {
                     window.location.reload();
                 }, 2500);
@@ -279,8 +308,10 @@ Game.Launch = function ()
                 if (Trophy[4] === 0) {
                     Trophy[4] = 1;
                     TrophyCount++;
-                    document.getElementById("toolTip").innerHTML = "ToolTip: Trophy: " + TrophyText[4];
-                    play();
+                    playcheat();
+                }
+                else {
+                    playcheat();
                 }
 
                 Game.coins += 20000000000;
